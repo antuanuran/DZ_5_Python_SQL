46ae9a2f918d26c8d56a0801a4bae91886dd8ac0
test <test@neto.com>
2023-02-07 11:52:24 +0300

    второй комит

--- Client.py
+++ Client.py
@@ -1,24 +1,33 @@
-
 def create_db(conn) -> None:
     cur = conn.cursor()
     cur.execute("""
     drop table if exists phones cascade;
     drop table if exists clients cascade;
+    drop table if exists find cascade;
 
     create table if not exists phones (
         id SERIAL primary key,
         operator varchar (60),       
         number varchar (60) unique
         );
-    
+
     create table if not exists clients (
         id SERIAL primary key,
         firstname varchar (60) not null,
         lastname varchar (60) not null,
         email varchar (60) not null unique,
         phone_id integer references phones (id));
+
+    create table if not exists find (
+        id SERIAL primary key,
+        firstname varchar (60) not null,
+        lastname varchar (60) not null,
+        email varchar (60) not null unique,
+        phone varchar (60) not null unique
+        );  
             """)
     conn.commit()
+
 
 # 1.Добавление телефона
 def add_phones(conn, operator, number) -> None:
@@ -30,6 +39,7 @@ def add_phones(conn, operator, number) -> None:
     """, (operator, number))
     conn.commit()
 
+
 # 2.Добавление клиента с телефоном
 def add_clients(conn, first_name, last_name, email, phone_id) -> None:
     cur = conn.cursor()
@@ -40,20 +50,47 @@ def add_clients(conn, first_name, last_name, email, phone_id) -> None:
     """, (first_name, last_name, email, phone_id))
     conn.commit()
 
+
 # 3.Изменение клиента
 def update_client(conn, first_name, last_name, email, phone_id, id_client):
     cur = conn.cursor()
     cur.execute("""
-        update clients set  firstname=%s, lastname=%s, email=%s, phone_id=%s
-        where clients.id =%s;
-        """, (first_name, last_name, email, phone_id, id_client))
+    update clients set  firstname=%s, lastname=%s, email=%s, phone_id=%s
+    where clients.id =%s;
+    """, (first_name, last_name, email, phone_id, id_client))
     conn.commit()
+
 
 # 4.Удаление телефона
 def remove_phone(conn, id_client):
     cur = conn.cursor()
     cur.execute("""
-        update clients set phone_id=%s
-        where clients.id =%s;
-        """, (None, id_client))
+    update clients set phone_id=%s
+    where clients.id =%s;
+    """, (None, id_client))
     conn.commit()
+
+
+# 5. Удаление клиента
+def remove_client(conn, id_client):
+    cur = conn.cursor()
+    cur.execute("""
+    delete from clients
+    where clients.id =%s;
+    """, id_client)
+    conn.commit()
+
+
+# 6. Поиск клиента и вывод в новую таблицу Find
+def find_client(conn, firstname, lastname, email, number):
+    cur = conn.cursor()
+    cur.execute("""
+    insert into find (firstname, lastname, email, phone)
+    select c.firstname, c.lastname, c.email, p.number from clients c 
+    join phones p on c.phone_id = p.id 
+    where c.firstname like %s or c.lastname like %s or c.email like %s or p.number like %s 
+    """, (firstname, lastname, email, number))
+    conn.commit()
+
+
+
--- Main.py
+++ Main.py
@@ -1,20 +1,30 @@
-from Client import create_db, add_clients, add_phones, update_client, remove_phone
+from Client import create_db, add_clients, add_phones, update_client, remove_phone, remove_client, find_client
 import psycopg2 as pg
 
 
 conn = pg.connect("dbname=Home_5 user=postgres password=postgres")
 create_db(conn)
 
-add_phones(conn, "МТС", "8-916-272-06-58")
-add_phones(conn, "Билайн", "8-908-252-06-59")
+add_phones(conn, "МТС", "8-916-272-06-66")
+add_phones(conn, "Билайн", "8-908-252-06-55")
 
-add_clients(conn, "Python", "Ураноvv_python", "antoha@mail.ru", 1)
-add_clients(conn, "Python", "Иванов_python", "ivan@mail.ru", 2)
-add_clients(conn, "Python", "Петров_python", "petrov@mail.ru", 1)
+add_clients(conn, "Уранов", "Антон", "antoha@mail.ru", 2)
+add_clients(conn, "Иванов", "Денис", "ivan@mail.ru", 2)
+add_clients(conn, "Петров", "Женя", "petrov@mail.ru", 1)
 
-update_client(conn, "SQL", "Доржиев", "Почта", "2", 1)
+update_client(conn, "Уранов 2", "Антон 2", "Почта 2", "2", 2)
 
-remove_phone(conn, 2)
+remove_phone(conn, 1)
+
+remove_client(conn, "1")
+
+find_client(conn, "УраноЖ", "", "petrov@mail.ru", "")
+
+
+
+
+
+
 
 
 
